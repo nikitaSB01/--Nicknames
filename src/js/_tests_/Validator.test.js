47431fb2148c966563e536956d878ceb5ee1efa6
nikitaSB01 <118895582+nikitaSB01@users.noreply.github.com>
2024-02-08 17:12:53 +0600

    доделал тесты

--- src/js/_tests_/Validator.test.js
+++ src/js/_tests_/Validator.test.js
@@ -3,13 +3,23 @@ import Validator from '../Validator';
 // начать тесты
 
 test('проверка на выдачу ошибки написания номера', () => {
-  expect(Validator('+8 = 9934482')).toThrow();
+  expect(() => Validator('+8 = 9934482')).toThrow();
 });
 
-/*
-test('проверка на правильность написания логина', () => {
-  const funcResult = new Validator('Nikita12S');
-  const result = 'Ваш логин: Nikita12S соответствует требованиям.';
-  expect(funcResult.validateUsername()).toEqual(result);
+test('проверка на правильность преобразования номера с 8 в начале', () => {
+  const funcResult = Validator('8 (927) 000-00-00');
+  const result = '+79270000000';
+  expect(funcResult).toEqual(result);
 });
- */
+
+test('проверка на правильность преобразования номера с +7 в начале', () => {
+  const funcResult = Validator('+7 960 000 00 00');
+  const result = '+79600000000';
+  expect(funcResult).toEqual(result);
+});
+
+test('проверка на правильность преобразования не РФ номера', () => {
+  const funcResult = Validator('+86 000 000 0000');
+  const result = '+860000000000';
+  expect(funcResult).toEqual(result);
+});
